d6074d650803d601950be769ddcf1d8e6d9b05fd
Lee Chase <lee.chase@uk.ibm.com>
2021-01-20 18:21:52 +0000

    feat: add carbon icon

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Bee32
+    class="test"
+    :class="testClass"
+    style="color: red;"
+    :style="testStyle"
+    title="I am a title"
+    data-other="data-other"
+    @click="handleClick"
+  />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import { Bee32 } from "@carbon/icons-vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Bee32
+  },
+  data() {
+    return {
+      testClass: "test-class",
+      testStyle: {
+        transform: "rotate(45deg)"
+      }
+    };
+  },
+  methods: {
+    handleClick() {
+      alert("click");
+    }
   }
 };
 </script>
@@ -22,5 +46,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.test {
+  background-color: yellow;
+}
+
+.test-class {
+  box-shadow: 0 0 0 5px green;
 }
 </style>
